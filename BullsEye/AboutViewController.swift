e8746bd9c9f3dbafa55ef893ab794daf47fdeade
snake0707 <lib0707@gmail.com>
2015-06-30 23:20:07 +0800

    06 - About Screen

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Li Bin on 15/6/30.
+//  Copyright (c) 2015年 LiBin. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBAction func close() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
